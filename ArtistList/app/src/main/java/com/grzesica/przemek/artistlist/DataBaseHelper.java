48360767a02a44e76e4818f3cc4f863315da37f0
Przemek Grzesica <przemek.grzesica@gmail.com>
2017-11-27 10:47:32 +0100

    Create HttpHandler

---
+++ ArtistList/app/src/main/java/com/grzesica/przemek/artistlist/DataBaseHelper.java
@@ -0,0 +1,38 @@
+package com.grzesica.przemek.artistlist;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by przemek on 22.11.17.
+ * SQLHelper for creation two types of databases (author list and album list of author)
+ */
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    DataBaseHelper(Context context, String dbName, int dbVersion){
+        super(context, dbName, null, dbVersion);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String authListString = "CREATE TABLE ARTIST_LIST(_id INTEGER PRIMARY KEY AUTOINCREMENT ,"
+                                + "NAME TEXT, DESCRIPTION TEXT, GENRES TEXT, "
+                                + "AUTHOR_IMAGE_ID INTEGER);";
+//        String albumListString = "CREATE TABLE ALBUM_LIST(_id INTEGER PRIMARY KEY AUTOINCREMENT ,"
+//                + "TITLE TEXT, ALBUM_IMAGE_ID INTEGER);";
+        db.execSQL(authListString);
+        for (int i :
+             ) {
+            
+        }
+        insertArtist.(db, strName, strDescr, strGenres, R.drawable.?);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
