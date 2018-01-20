7c5ce7a0571f1a1180a1cfac4eaea8d4c78db3cd
przemek.grzesica <przemek.grzesica@gmail.com>
2018-01-20 19:13:58 +0100

    Get methods

---
+++ ArtistList/app/src/main/java/com/grzesica/przemek/artistlist/AlbumsListActivity.java
@@ -0,0 +1,13 @@
+package com.grzesica.przemek.artistlist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AlbumsListActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.album_artist_list);
+    }
+}
