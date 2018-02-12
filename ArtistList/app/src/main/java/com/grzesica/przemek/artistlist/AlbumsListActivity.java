ece17907e5bb78ed56ed509049939480ff82fd30
Przemek Grzesica <przemek.grzesica@gmail.com>
2018-02-12 14:47:06 +0100

    Both activities manu bars done

--- ArtistList/app/src/main/java/com/grzesica/przemek/artistlist/AlbumsListActivity.java
+++ ArtistList/app/src/main/java/com/grzesica/przemek/artistlist/AlbumsListActivity.java
@@ -5,6 +5,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -27,6 +30,8 @@ public class AlbumsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.album_list_activity);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.albumsToolbar);
+        setSupportActionBar(toolbar);
 
         long artistDataId = (long) getIntent().getExtras().get(strArtistDataId);
         dbAdapter = new DataBaseAdapter(getApplicationContext());
@@ -79,7 +84,6 @@ public class AlbumsListActivity extends AppCompatActivity {
         lvAlbums.setAdapter(albumsListAdapter);
     }
 
-
     private Cursor getAlbumTable(String position) {
         dbCursor = dbAdapter.getAlbumsListItems(position);
         if (dbCursor != null) {
@@ -87,5 +91,27 @@ public class AlbumsListActivity extends AppCompatActivity {
             dbCursor.moveToFirst();
         }
         return dbCursor;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.album_list_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.artistToolbar) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
