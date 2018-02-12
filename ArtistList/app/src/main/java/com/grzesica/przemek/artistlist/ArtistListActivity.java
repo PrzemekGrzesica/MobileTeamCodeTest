dd1bed852264f9c1a6e7c5d1e73396aa2c8c26fd
Przemek Grzesica <przemek.grzesica@gmail.com>
2018-02-12 13:08:15 +0100

    Artist activity menu leyout

--- ArtistList/app/src/main/java/com/grzesica/przemek/artistlist/ArtistListActivity.java
+++ ArtistList/app/src/main/java/com/grzesica/przemek/artistlist/ArtistListActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -19,6 +22,8 @@ public class ArtistListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.artist_list_activity);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.artistToolbar);
+        setSupportActionBar(toolbar);
         initUiElements();
     }
 
@@ -64,5 +69,26 @@ public class ArtistListActivity extends AppCompatActivity {
                 };
         lvArtist = (ListView) findViewById(R.id.artistListView);
         lvArtist.setOnItemClickListener(itemClickListener);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.artist_list_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.artistToolbar) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
